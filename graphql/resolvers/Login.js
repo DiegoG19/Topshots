743f619d8354a591605eac55a574834e6fb68e97
Diego G <gd369234@gmail.com>
2019-11-22 17:58:17 -0600

    added message in login.js res

--- graphql/resolvers/Login.js
+++ graphql/resolvers/Login.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
-
+// creates a check if there is a user with the same info and checks if the exits in the cluster server
 module.exports = {
   createUser: async args => {
     try {
@@ -11,7 +11,7 @@ module.exports = {
         throw new Error('User exists already.');
       }
       const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
-
+// creates new user and requirements 
       const user = new User({
         email: args.userInput.email,
         password: hashedPassword
@@ -24,6 +24,7 @@ module.exports = {
       throw err;
     }
   },
+  // login creation and what fields it takes 
   login: async ({ email, password }) => {
     const user = await User.findOne({ email: email });
     if (!user) {
@@ -40,6 +41,7 @@ module.exports = {
         expiresIn: '1h'
       }
     );
+    // return everything 
     return { userId: user.id, token: token, tokenExpiration: 1 };
   }
 };
